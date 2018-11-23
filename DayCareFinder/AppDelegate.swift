8ab62ed5a1010e94b1b529e85c2f2c6dcdd70aab
Jared Payne <jaredmpayne@gmail.com>
2018-11-23 15:30:11 -0600

    Start adding Core Data
    This doesn't fully implement it, but the basic files are there.
    I'll finish adding it once I fix the DayCares storyboard.

--- DayCareFinder/AppDelegate.swift
+++ DayCareFinder/AppDelegate.swift
@@ -6,9 +6,28 @@
 //  Copyright © 2018 Jared Payne. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
 @UIApplicationMain public class AppDelegate: UIResponder, UIApplicationDelegate {
 
     public var window: UIWindow?
+    
+    public lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DayCareFinder")
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    public func applicationDidFinishLaunching(_ application: UIApplication) {
+        
+    }
+    
+    public func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
 }
