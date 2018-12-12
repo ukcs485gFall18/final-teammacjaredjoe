aff82a78213001e5729c56c4e4354cb5d71ad5f3
John McNerney <jmacmcnerney@gmail.com>
2018-12-12 14:52:45 -0500

    Comments

--- DayCareFinder/Controllers/Enrollments/EnrollmentsCollectionViewController.swift
+++ DayCareFinder/Controllers/Enrollments/EnrollmentsCollectionViewController.swift
@@ -16,6 +16,7 @@ public class EnrollmentsCollectionViewController: UICollectionViewController {
     
     public var kids: [Kid] = []
     
+    //load page
     public override func viewDidLoad() {
         //super calls the parent classes' method
         super.viewDidLoad()
@@ -25,10 +26,12 @@ public class EnrollmentsCollectionViewController: UICollectionViewController {
 
     }
     
+    //return kids count
     public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.kids.count
     }
     
+    //path to specific kid and return cell of the kid
     public override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellType.reuseIdentifier, for: indexPath) as! CellType
         let kid = self.kids[indexPath.row]
@@ -36,6 +39,7 @@ public class EnrollmentsCollectionViewController: UICollectionViewController {
         return cell
     }
     
+    //unwind segue
     public override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let enrollment = Enrollment()
         enrollment.dayCareId = self.dayCare.id
@@ -51,6 +55,7 @@ public class EnrollmentsCollectionViewController: UICollectionViewController {
         }
     }
     
+    //update and refresh page
     @objc private func updateData() {
         User.currentUser!.getKids() { data, response, error in
             self.kids = User.currentUser!.kids ?? []
