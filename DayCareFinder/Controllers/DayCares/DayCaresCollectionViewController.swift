da2e63d6e3cf2a3fd6b4a054b8bbc76b3a3bba68
Joe Clements <joeyclements89@yahoo.com>
2018-12-11 20:23:19 -0500

    added background detail for empty collection views to clear up user ambiguity

--- DayCareFinder/Controllers/DayCares/DayCaresCollectionViewController.swift
+++ DayCareFinder/Controllers/DayCares/DayCaresCollectionViewController.swift
@@ -101,3 +101,22 @@ public class DayCaresCollectionViewController: UICollectionViewController {
         }
     }
 }
+
+extension UICollectionViewController {
+    
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0;
+        messageLabel.textAlignment = .center;
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+        messageLabel.sizeToFit()
+        
+        self.backgroundView = messageLabel;
+    }
+    
+    func restore() {
+        self.backgroundView = nil
+    }
+}
